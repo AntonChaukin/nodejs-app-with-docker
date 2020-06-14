36ee927d4f9614f0e73a1bd321c833023c678fd1
anton <achaukin@netw.fr>
2020-06-14 20:40:06 +0300

    Revert "add simple test"
    This reverts commit 6e2114a4

--- test/test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-describe('first test', () => {
-    axios.defaults.baseURL = 'http://127.0.0.1:4001'
-    test('get visitors', async () => {
-        const res = await axios.get('/');
-        expect(res.data).toEqual('Number of visits: 1');
-    });
-})
